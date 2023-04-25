2568adacde2ba1d1be6de63c52ec5614c8c624c8
maaran <maaranfamily22@gmail.com>
2023-04-25 18:46:26 +0530

    waiver update incom

--- src/app/Components/AccountsController/Tally/tally/tally.component.ts
+++ src/app/Components/AccountsController/Tally/tally/tally.component.ts
@@ -45,13 +45,13 @@ export class TallyComponent implements OnInit {
   setAll(completed: boolean, item: any) {
     console.log("before", this.selectedInvoices)
     if (completed == true) {
-      this.selectedInvoices.push(item.id)
+      this.selectedInvoices.push(item)
     }
     else {
 
-      if (this.selectedInvoices.find(x => x == item.id)) {
+      if (this.selectedInvoices.find(x => x.id == item.id)) {
         this.selectedInvoices = this.selectedInvoices.filter(x => {
-          if (x != item.id) {
+          if (x.id!= item.id) {
             return item
           }
         })
@@ -66,7 +66,7 @@ setExchangeHeader() {
     this.dataSource.data.filter((y:any)=>{
       skip=false;
       this.selectedInvoices.forEach(x=>{
-      if(y.id===x){
+      if(y.id===x.id){
         temparray.push({  ...y,
           exchangeRate:this.exchangeHeader,
         isSelected:true})
@@ -224,7 +224,7 @@ updateintegration(){
 
 var invintigxchange = this.selectedInvoices.map(x => {
 return { 
-  "id": x,
+  "id": x.id,
   "invoiceNo": "",
   "exchangeRate": this.exchangeHeader,
   "invintigxchange": []
@@ -243,4 +243,77 @@ this.http.post<any>('https://localhost:7208/api/Invoice/GetExchangeRatetoInvoice
 })
 }
 
+
+movetointegration(){
+
+  let invintiglist=  this.selectedInvoices.map(x =>{
+    return {
+      "id": x.id,
+      "invoiceNo": x.invoiceNo,
+      "invoiceDate": x.invoiceDate,
+      "estInvoiceDate": x.estInvoiceDate,
+      "referenceDate": x.referenceDate,
+      "shortName": x.shortName,
+      "description": x.description,
+      "productValue": x.productValue,
+      "discount": x.discount,
+      "invoiceValue": x.invoiceValue,
+      "createdUTC": x.createdUTC,
+      "createdBy": 0,
+      "employeeId": 0,
+      "updatedBy": 0,
+      "invintiglist": [],
+      "updatedUTC": x.updatedUTC,
+      "primaryDateTime": x.primaryDateTime,
+      "secondaryDateTime": x.secondaryDateTime,
+      "artInvoiceAmount": x.artInvoiceAmount,
+      "digiInvoiceAmount": x.digiInvoiceAmount,
+      "roundOff": x.roundOff,
+      "artWaiver": x.artWaiver,
+      "digiWaiver": x.digiWaiver,
+      "waiver": x.waiver,
+      "receivableValue": x.receivableValue,
+      "totalInvoiceValue": x.totalInvoiceValue,
+      "transactionId": x.transactionId,
+      "exchangeRate": x.exchangeRate
+    }
+    
+  
+  })
+  let data = {
+    "id": 0,
+    "invoiceNo": "string",
+    "invoiceDate": "2023-04-25T12:46:24.137Z",
+    "estInvoiceDate": "2023-04-25T12:46:24.137Z",
+    "referenceDate": "2023-04-25T12:46:24.137Z",
+    "shortName": "string",
+    "description": "string",
+    "productValue": 0,
+    "discount": 0,
+    "invoiceValue": 0,
+    "createdUTC": "2023-04-25T12:46:24.137Z",
+    "createdBy": 0,
+    "employeeId": 152,
+    "updatedBy": 0,
+    "invintiglist":invintiglist,
+    "updatedUTC": "2023-04-25T12:46:24.137Z",
+    "primaryDateTime": "2023-04-25T12:46:24.137Z",
+    "secondaryDateTime": "2023-04-25T12:46:24.137Z",
+    "artInvoiceAmount": 0,
+    "digiInvoiceAmount": 0,
+    "roundOff": 0,
+    "artWaiver": 0,
+    "digiWaiver": 0,
+    "waiver": 0,
+    "receivableValue": 0,
+    "totalInvoiceValue": 0,
+    "transactionId": 0,
+    "exchangeRate": 0
+  }
+  this.http.post<any>('https://localhost:7208/api/Invoice/GetCopytoIntegration',data).subscribe(data =>{
+    console.log(data,"movedata");
+    this.selectedInvoices = [];
+    this.onSubmit();
+  })
+}
 }
