a74e393d70fb3971afe5e11f50d013b7c2e1d9da
maaran <maaranfamily22@gmail.com>
2023-04-26 15:51:34 +0530

    Tally and waiver and advanceadjustment completed

--- src/app/Components/AccountsController/AdvanceAdjustment/Edit/editadvanceadjustment/editadvanceadjustment.component.ts
+++ src/app/Components/AccountsController/AdvanceAdjustment/Edit/editadvanceadjustment/editadvanceadjustment.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { AdvanceadjustmentService } from 'src/app/Services/AccountController/AdvanceAdjustment/advanceadjustment.service';
 import { AdvanceadjustmentComponent } from '../../Index/advanceadjustment/advanceadjustment.component';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CoreService } from 'src/app/Services/CustomerVSEmployee/Core/core.service';
 
 
 @Component({
@@ -13,14 +14,13 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./editadvanceadjustment.component.scss']
 })
 export class EditadvanceadjustmentComponent {
-  
-  
   itemForm: FormGroup;
-
+  invoiceForm: FormGroup;
 
 
 
   //Customerdropdownvalues dropdowndeclaration
+  InvoiceId:any=0;
   selectedInvoiceOption: any = '';
   Invoicedropdownvalues: any[] = [];
   invoiceValue: any;
@@ -31,7 +31,7 @@ export class EditadvanceadjustmentComponent {
   input6: any;
 
   constructor(
-    private index:AdvanceadjustmentComponent,private advanceservice:AdvanceadjustmentService,private _fb: FormBuilder,private http: HttpClient,
+    private index:AdvanceadjustmentComponent,private _coreService:CoreService,private advanceservice:AdvanceadjustmentService,private formBuilder: FormBuilder,private http: HttpClient,
     public dialogRef: MatDialogRef<EditadvanceadjustmentComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
   ) { console.log(data,"indexdata");
@@ -46,24 +46,37 @@ export class EditadvanceadjustmentComponent {
 
   ngOnInit(): void {
     // department dropdown fetch the values from the API
-    this.http.get<any>('https://localhost:7208/api/Receivable/GetCustomerInvoice?CustomerId=4141').subscribe(invoicedata => {
+    this.http.get<any>(`https://localhost:7208/api/Receivable/GetCustomerInvoice?CustomerId=${this.data.department}`).subscribe(invoicedata => {
       this.Invoicedropdownvalues = invoicedata;
+    });
+
+    //formvalues
+    this.invoiceForm = this.formBuilder.group({
+      selectedinvoicenumber:  [''],
+      invoicevalue: [{value: '', disabled: true}],
+      alreadyadjustedamount:  [{value: '', disabled: true}],
+      balanceamount:  [{value: '', disabled: true}],
+      InvoiceDate:  [{value: '', disabled: true}],
+      AdvanceAmount:  [{value: '', disabled: true}],
+      enteramount: [0]
     });
   }
 
+  
 
   onchangevalues() {
-    this.onSelectionChange(11, 23);
+    this.onSelectionChange();
   }
 
   advanceamount = this.data.availableAdvance;
-  onSelectionChange(invoiceid: number, customerid: number) {
+  onSelectionChange() {
     this.http.get<any>(`https://localhost:7208/api/Receivable/GetInvoice?invoiceNo=${this.selectedInvoiceOption}&customerId=${this.data.department}`).subscribe(data => {
       this.invoiceValue = data.invoiceValue;
       this.adjustmentAmount = data.adjustmentAmount;
       this.input3 = data.input3;
       this.input4 = data.invoiceDate;
       this.InvoiceAmount = this.advanceamount;
+      this.InvoiceId=data.id
     });
   }
 
@@ -79,6 +92,8 @@ export class EditadvanceadjustmentComponent {
   items: any[] = [];
    
   addItem(itemForm) {
+    console.log(itemForm,"itemform");
+
     if (!itemForm.valid) {
       return; // prevent adding the item if the form is not valid
     }
@@ -86,7 +101,9 @@ export class EditadvanceadjustmentComponent {
     if (!this.input6) {
       return alert("Field is required"); // prevent adding the item if the quantity field is empty
     }
-    this.items.push({ invoicetype: this.invoicetype, selectedValue: this.selectedValue,invoiceValue:this.invoiceValue,adjustmentAmount:this.adjustmentAmount,input3:this.input3,input4:this.input4,InvoiceAmount:this.InvoiceAmount,input6:this.input6 });
+
+    this.items.push({ invoicetype: this.invoicetype, selectedValue: this.selectedValue,invoiceValue:this.invoiceValue,adjustmentAmount:this.adjustmentAmount,input3:this.input3,input4:this.input4,InvoiceAmount:this.InvoiceAmount,input6:this.input6 ,id:this.InvoiceId});
+    this.InvoiceId=0,
     this.invoicetype = '';
     this.selectedValue;
     this.invoiceValue,
@@ -99,13 +116,42 @@ export class EditadvanceadjustmentComponent {
     
   }
 
+ amountvalidationerror:boolean= true;
+
+  amountvalidation() {
+    // console.log("eenter the values by the user changes");
+    
+    const enteredAmount = this.invoiceForm.get('enteramount')?.value;
+    console.log("Enter the amount",this.InvoiceAmount);
+   
+    if (this.input6 > this.InvoiceAmount) {
+      this.amountvalidationerror = true;
+    }
+    else{
+      this.amountvalidationerror = false;
+    }
+  }
+
     deleteItem(index: number) {
       this.items.splice(index, 1);
     }
-
-
-    formsubmit(){
-      console.log(this.itemForm.value,"AdvanceAdjustment")
+    onFormSubmit(){
+    console.log(this.items,"Advanceadjustemnt");
+  let temparray=this.items.map(x =>{
+    return  {
+      "invoiceId": x.id,
+      "invoiceNo": x.selectedValue,
+      "adjustmentAmount": x.adjustmentAmount
     }
-
+  })
+    let data = {
+      "receivableAdjustments": temparray,
+      "advanceId": this.data.id
+    }
+      this.http.post<any>('https://localhost:7208/api/AdvanceAdjustment/CreateAdvanceAdjustment',data).subscribe(data=>{
+        console.log(data,"formsubmission");
+        this._coreService.openSnackBar('Employee added successfully');
+       this.dialogRef.close();
+      })
+    }
 }
