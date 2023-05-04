999d2173ff55fcd5a8cbb8b366d3a0db0f124937
maaranpetchi <maaranfamily22@gmail.com>
2023-05-04 09:40:02 +0530

    ProductionALlocation UI Completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TopnavbarComponent } from './Components/Navigation/TopNavbar/topnavbar/topnavbar.component';
-import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 const routes: Routes = [
  
