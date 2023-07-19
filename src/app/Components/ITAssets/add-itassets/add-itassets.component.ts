a53e25038a82a5ac0c13a420a7c824f92eb409d2
maaranpetchi <maaranfamily22@gmail.com>
2023-07-19 18:42:12 +0530

    additassets

--- src/app/Components/ITAssets/add-itassets/add-itassets.component.ts
+++ src/app/Components/ITAssets/add-itassets/add-itassets.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { CoreService } from 'src/app/Services/CustomerVSEmployee/Core/core.service';
+import { LoginService } from 'src/app/Services/Login/login.service';
+import { SpinnerService } from '../../Spinner/spinner.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-add-itassets',
@@ -13,7 +22,33 @@ export class AddItassetsComponent implements OnInit {
   ngOnInit(): void {
   
   }
-//NgModel to save the values
+  constructor(private http: HttpClient, @Inject(MAT_DIALOG_DATA) public data: any, private _coreService: CoreService, private loginservice: LoginService, private spinnerService: SpinnerService,  private router: Router) {}
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;  
+  displayedColumns: string[] = ['baynumber', 'software', 'softwarestatus','Action'];
+  employeeFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteEmployee(id: number) {
+    // this.spinnerService.requestStarted();
+
+    // this.checklistservice.deleteEmployee(id).subscribe({
+    //   next: (res) => {
+    //     this.spinnerService.requestEnded();
+
+    //     this._coreService.openSnackBar('Employee deleted!', 'done');
+    //     this.fetchtableData();
+    //   },
+    //   error: console.log,
+    // });
+  }
+  //NgModel to save the values
   Type:number;
   BayNo: string; 
   Location: string; 
@@ -63,6 +98,9 @@ export class AddItassetsComponent implements OnInit {
 //NgModel to save the values
 
 
-//Form 
+//second Form 
+SoftwareId: number[];
+SoftwareData: any[] = []; 
+SoftwareStatus: number;
 
 }
