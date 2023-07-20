7e6d43c7b733dce56598f0774d52e6858d3d8674
maaranpetchi <maaranfamily22@gmail.com>
2023-07-20 14:34:24 +0530

    Itasset add completed in first page

--- src/app/Components/ITAssets/add-itassets/add-itassets.component.ts
+++ src/app/Components/ITAssets/add-itassets/add-itassets.component.ts
@@ -9,7 +9,18 @@ import { SpinnerService } from '../../Spinner/spinner.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { environment } from 'src/Environments/environment';
 
+
+//Interface for servertype
+interface GETSTD {
+  id: string;
+  description: string;
+}
+interface GETWSD {
+  id: string;
+  description: string;
+}
 @Component({
   selector: 'app-add-itassets',
   templateUrl: './add-itassets.component.html',
@@ -18,15 +29,17 @@ import { MatSort } from '@angular/material/sort';
 export class AddItassetsComponent implements OnInit {
   hardwareStepFormGroup: FormGroup;
   softwareStepFormGroup: FormGroup;
-  
+
+
+
   ngOnInit(): void {
-  
+    this.getPcType();
   }
-  constructor(private http: HttpClient, @Inject(MAT_DIALOG_DATA) public data: any, private _coreService: CoreService, private loginservice: LoginService, private spinnerService: SpinnerService,  private router: Router) {}
+  constructor(private http: HttpClient, private _coreService: CoreService, private loginservice: LoginService, private spinnerService: SpinnerService, private router: Router) { }
   dataSource: MatTableDataSource<any>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;  
-  displayedColumns: string[] = ['baynumber', 'software', 'softwarestatus','Action'];
+  @ViewChild(MatSort) sort: MatSort;
+  displayedColumns: string[] = ['baynumber', 'software', 'softwarestatus', 'Action'];
   employeeFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -49,58 +62,167 @@ export class AddItassetsComponent implements OnInit {
     // });
   }
   //NgModel to save the values
-  Type:number;
-  BayNo: string; 
-  Location: string; 
-  baynodisable: boolean; 
+  Type: number;
+  BayNo: string;
+  Location: string;
+  baynodisable: boolean = false;
 
 
-  PcName: string; 
-  PcType: any; 
+  PcName: string;
+  PcType: any;
   HardwareData: any[] = [];
   PcTypeV: any;
 
-  Monitor: string; 
-  MonitorSno: string; 
+  Monitor: string;
+  MonitorSno: string;
   keyboard: string;
   keyboardSno: string;
 
-  Roll: string; 
+  Roll: string;
   Division: string;
 
   Brand: string;
   Model: string;
 
-  WarrantyDetails: string; 
+  WarrantyDetails: string;
   RAM: string;
 
   Processor: string;
   Graphics: string;
- 
-  HDD: string; 
+
+  HDD: string;
   TAGNumber: string;
 
-  MacAddress: string; 
+  MacAddress: string;
   OS: string;
 
-  IPAddress: string; 
-  ServerType: any; 
-  GetSTD: any[] =[];
+  IPAddress: string;
+  ServerType: any;
+  GetSTD: GETSTD[] = [
+    { id: '1', description: "Server" },
+    { id: '2', description: "Virtual Server" }
+  ];
   ServerTypeV: any;
 
-  InvoiceDate: Date; 
+  InvoiceDate: Date;
   InvoiceNumber: string;
-  Mouse: string; 
-  MouseSNO: string; 
-  WorkingStatus: any; 
-  GetWSD: any[] = [];
+  Mouse: string;
+  MouseSNO: string;
+  WorkingStatus: any;
+  GetWSD: GETWSD[] = [
+    { id: '1', description: "Active" },
+    { id: '2', description: "Repair" },
+    { id: '3', description: "Stand By" }
+  ];
   WorkingStatusV: any;
-//NgModel to save the values
+  //NgModel to save the values
 
 
-//second Form 
-SoftwareId: number[];
-SoftwareData: any[] = []; 
-SoftwareStatus: number;
+  //second Form 
+  SoftwareId: number[];
+  SoftwareData: any[] = [];
+  SoftwareStatus: number;
 
+
+  //method to get the pctype dropdown
+  getPcType() {
+    this.http.get<any>(environment.apiURL + `ITAsset/nGetHardwareSoftware`).subscribe(results => {
+      this.HardwareData = results.hardwareData;
+    });
+  }
+
+  //mrthod to show the instock in input field
+  onTypeChange() {
+    if (this.Type === 2) { // If the selected type is "InStock" (value = 2)
+      // Set the BayNo field with the values of InStock
+      this.BayNo = 'Instock';
+      // Disable the BayNo input field
+      this.baynodisable = true;
+    } else {
+      // If the selected type is anything other than "InStock"
+      // Reset the BayNo field to an empty value
+      this.BayNo = '';
+      // Enable the BayNo input field
+      this.baynodisable = false;
+    }
+  }
+
+  firstNext() {
+    console.log("enetr the first");
+    
+    // let payload = {
+    //   BayNumber: this.BayNo,
+    //   Brand: this.Brand,
+    //   Division: this.Division,
+    //   EmployeeId: this.loginservice.getUsername(),
+    //   Graphics: this.Graphics,
+    //   HardwareId: "2",
+    //   Hdd: this.HDD,
+    //   InvoiceDate: this.InvoiceDate,
+    //   InvoiceNumber: this.InvoiceNumber,
+    //   IpAddress: this.IPAddress,
+    //   Keyboard: this.keyboard,
+    //   KeyboardSerialNumber: this.keyboardSno,
+    //   Location: this.Location,
+    //   MacAddress: this.MacAddress,
+    //   Model: this.Model,
+    //   Monitor: this.Monitor,
+    //   MonitorSerialNumber: this.MonitorSno,
+    //   Mouse: this.Mouse,
+    //   MouseSerialNumber: this.MouseSNO,
+    //   Os: this.OS,
+    //   PcName: this.PcName,
+    //   Processor: this.Processor,
+    //   Ram: this.RAM,
+    //   Roll: this.Roll,
+    //   ServerTypeId: this.ServerType,
+    //   TagNumber: this.TAGNumber,
+    //   WarantyDetails: this.WarrantyDetails,
+    //   WorkingStatusId: this.WorkingStatus
+    // }
+
+
+    let payloadupload={
+      "id": 0,
+      "employeeId":this.loginservice.getUsername(),
+      "BayNumber": this.BayNo,
+      "Brand": this.Brand,
+      "Division": this.Division,
+      "EmployeeId": this.loginservice.getUsername(),
+      "Graphics": this.Graphics,
+      "HardwareId": "2",
+      "Hdd": this.HDD,
+      "InvoiceDate": this.InvoiceDate,
+      "InvoiceNumber": this.InvoiceNumber,
+      "IpAddress": this.IPAddress,
+      "Keyboard": this.keyboard,
+      "KeyboardSerialNumber": this.keyboardSno,
+      "Location": this.Location,
+      "MacAddress": this.MacAddress,
+      "Model": this.Model,
+      "Monitor": this.Monitor,
+      "MonitorSerialNumber": this.MonitorSno,
+      "Mouse": this.Mouse,
+      "MouseSerialNumber": this.MouseSNO,
+      "Os": this.OS,
+      "PcName": this.PcName,
+      "Processor": this.Processor,
+      "Ram": this.RAM,
+      "Roll": this.Roll,
+      "ServerTypeId": this.ServerType,
+      "TagNumber": this.TAGNumber,
+      "WarantyDetails": this.WarrantyDetails,
+      "WorkingStatusId": this.WorkingStatus,
+      "softwareId": [],
+      "softwareStatusId": 0,
+      "serverType": "",
+      "workingStatus": "",
+
+    }
+    this.http.post<any>(environment.apiURL +`ITAsset/nSetITHData`,payloadupload).subscribe(results =>{
+      this._coreService.openSnackBar("Data Added successfully!");
+    });
+    console.log(payloadupload,"payloadinfirstpage");
+    
+  }
 }
