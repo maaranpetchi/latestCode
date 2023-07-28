b02035cb0022e94fc4ab079983dda0016171ea06
Petchiappan.P <Petchiappan.P@KTP-KIT008>
2023-07-28 17:56:56 +0530

    Side Navbar Completed

--- src/app/Components/Navigation/TopNavbar/topnavbar/topnavbar.component.ts
+++ src/app/Components/Navigation/TopNavbar/topnavbar/topnavbar.component.ts
@@ -8,6 +8,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 import { environment } from 'src/Environments/environment';
+import { SpinnerService } from 'src/app/Components/Spinner/spinner.service';
 
 
 @Component({
@@ -26,15 +27,19 @@ import { environment } from 'src/Environments/environment';
     ])
   ]
 })
-export class TopnavbarComponent  implements OnInit{
-constructor(private _dialog: MatDialog,private loginservice:LoginService ,private router: Router, private logoutService:LogoutService,private cookieService: CookieService,private http:HttpClient){}
+export class TopnavbarComponent implements OnInit {
+  isAdmin: any;
+  menus: any;
+  showingMenu: any;
+  constructor(private _dialog: MatDialog, private loginservice: LoginService, private router: Router, private logoutService: LogoutService, private cookieService: CookieService, private http: HttpClient, private spinnerService: SpinnerService) { }
   ngOnInit(): void {
     this.getProcesses();
+    this.checkIsAdmin();
   }
 
 
-//toggle 
-isSidenavOpen = true;
+  //toggle 
+  isSidenavOpen = true;
 
   toggleSidenav() {
     this.isSidenavOpen = !this.isSidenavOpen;
@@ -43,14 +48,14 @@ isSidenavOpen = true;
 
 
 
-  getloginusername():string{
+  getloginusername(): string {
     return this.loginservice.getToken();
   }
 
-  toggleNav(nav:any){
-    if(nav.opened){
+  toggleNav(nav: any) {
+    if (nav.opened) {
       nav.close()
-    }else{
+    } else {
       nav.open();
     }
   }
@@ -81,64 +86,145 @@ isSidenavOpen = true;
 
   }
 
-Processes:any[] = [];
-  getProcesses(){
-this.http.get<any>(environment.apiURL+`Account/getEmployeeProcess/${this.loginservice.getUsername()}`).subscribe(data => {
-  this.Processes = data.employeeProcess;
-});
+  Processes: any[] = [];
+  getProcesses() {
+    this.http.get<any>(environment.apiURL + `Account/getEmployeeProcess/${this.loginservice.getUsername()}`).subscribe(data => {
+      this.Processes = data.employeeProcess;
+    });
   }
 
-  routeNav(process:any){
-if(process.id==1){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/clientindex']);
-}
-else if(process.id==2){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/production']);
-}
-else if(process.id==3){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/productionmain']);
-}
-else if(process.id==4){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/qualityallocation']);
-}
-else if(process.id==5){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/quality']);
-}
-else if(process.id==6){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/proofreadingallocation']);
-}
-else if(process.id==7){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/proofreading']);
-}
-else if(process.id==9){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/buddyproof']);
-}
-else if(process.id==11){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/sewout']);
-}
-else if(process.id==1013){
-  this.cookieService.set('processId', process.id);
-  this.cookieService.set('processName', process.name);
-  this.router.navigate(['/topnavbar/Reports']);
-}
+  routeNav(process: any) {
+    if (process.id == 1) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/clientindex']);
+    }
+    else if (process.id == 2) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/production']);
+    }
+    else if (process.id == 3) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/productionmain']);
+    }
+    else if (process.id == 4) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/qualityallocation']);
+    }
+    else if (process.id == 5) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/quality']);
+    }
+    else if (process.id == 6) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/proofreadingallocation']);
+    }
+    else if (process.id == 7) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/proofreading']);
+    }
+    else if (process.id == 9) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/buddyproof']);
+    }
+    else if (process.id == 11) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/sewout']);
+    }
+    else if (process.id == 1013) {
+      this.cookieService.set('processId', process.id);
+      this.cookieService.set('processName', process.name);
+      this.router.navigate(['/topnavbar/Reports']);
+    }
   }
 
+
+
+
+  //////////////////////SideNavbar////////////////////////////
+  // checkIsAdmin() {
+  //   // this.loader = true;
+  //   this.spinnerService.requestStarted();
+  //   this.http.get<any>(environment.apiURL + `Account/checkIsAdmin/${this.loginservice.getUsername()}`).subscribe(
+  //     (data) => {
+  //       this.spinnerService.requestEnded();
+  //       this.isAdmin = data.success;
+  //       this.menus = data.menu;
+
+  //       if (this.menus != null && this.menus.length > 0) {
+  //         this.menus = this.menus.split(',');
+  //         this.menus.forEach((menu) => {
+  //           menu = menu.replace('|', '_').replace('|', '_');
+  //           this.showingMenu = menu;
+  //           console.log(this.showingMenu,"ShowingMenu");
+  //           this[menu] = true;
+  //         });
+  //       }
+  //     },
+  //     (error) => {
+  //       console.error('Error checking admin status:', error);
+  //     },
+  //     () => {
+  //       this.spinnerService.resetSpinner();
+  //     }
+  //   );
+  // }
+  // Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+permission:any[]=[]
+checkIsAdmin() {
+  // this.loader = true;
+  this.spinnerService.requestStarted();
+  this.http.get<any>(environment.apiURL + `Account/checkIsAdmin/${this.loginservice.getUsername()}`).subscribe(
+    (data) => {
+      this.spinnerService.requestEnded();
+      this.isAdmin = data.success;
+      this.permission = this.GetMenuPermission(data.menu);
+    },
+    (error) => {
+      console.error('Error checking admin status:', error);
+    },
+    () => {
+      this.spinnerService.resetSpinner();
+    }
+  );
+}
+ GetMenuPermission(val:string){
+    
+  if(val){
+
+    val=val.replaceAll('|','');
+    const b=val.split(',');
+    const c=b.map(x=>parseInt(x))
+    console.log(c);
+    return c;
+  }
+  return [];
+  }
+  
+
+   checkIsTrue(inputarray,isAdmin){
+      let result=false
+      if(isAdmin){
+          result=true;
+      }
+      if(!result){
+      this.permission.forEach(x=>{
+          if(inputarray.includes(x)){
+              result=true;
+          
+          }
+      })
+      }
+      return result;
+  }
+  
 }
--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/Environments/environment';
+import { LoginService } from 'src/app/Services/Login/login.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,9 +14,55 @@ export class DashboardComponent implements OnInit {
   public secondValue: string='';
   public thirdValue: string='';
   public isChecked: boolean=true;
-  constructor() { }
+  process: { Id: any; Name: any; };
+  constructor(private http:HttpClient,private router:Router,private loginservice:LoginService) { }
 
   ngOnInit(): void {
   }
+  employeeProcess:any;
 
+
+
+  getData(){
+    this.http.get<any>(environment.apiURL+`Account/getEmployeeProcess/${this.loginservice.getUsername()}`).subscribe(results =>{
+      this.employeeProcess = results.employeeProcess
+      if (this.employeeProcess != null) {
+        if (this.employeeProcess.length > 1) {
+        }
+        else if (this.employeeProcess.length == 1) {
+            this.process = {
+                Id: this.employeeProcess[0].id,
+                Name: this.employeeProcess[0].name
+            };
+            if (this.process.Name != "Reports")
+            {
+                this.gotoProcess(process);
+            }
+        }
+    }
+    else {
+        alert('This Employee don\'t have permission')
+    }
+      });
+  }
+
+  gotoProcess(process: any) {
+    localStorage.setItem('processId', process.id);
+    localStorage.setItem('processName', process.name);
+
+    if (process.id == 1) {
+      this.router.navigate(['/topnavbar/clientindex']);
+    } else if (process.id == 2 || process.id == 4 || process.id == 6) {
+      this.router.navigate(['/PendingJobList']);
+    } else if (process.id == 3 || process.id == 5 || process.id == 7 || process.id == 9 || process.id == 11) {
+      this.router.navigate(['/Workflow']);
+    } else if (process.Name == "Reports") {
+      this.openReportsWindow();
+    }
+  }
+
+  openReportsWindow() {
+    const UserId = localStorage.getItem('UserId');
+    window.open('http://localhost:61928/#/Login?id=' + UserId, '_blank');
+  }
 }
